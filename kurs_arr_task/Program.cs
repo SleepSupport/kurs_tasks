c1171c5f334b0936bdceebba42c4d2307a443ae7
SleepSupport <79420757+SleepSupport@users.noreply.github.com>
2021-02-26 19:12:41 +0300

    дз №4

---
+++ kurs_arr_task/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace kurs_arr_task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrSortTest = new int[6];
+            Console.WriteLine("Введите значение массива int на 6 значений");
+            for (int i = 0; i < arrSortTest.Length; i++)
+            {
+                    arrSortTest[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("-----------Печать массива до сортировки---------");
+            foreach (int a in arrSortTest)
+            {
+                Console.WriteLine(a);
+            }
+            Array.Sort(arrSortTest);
+            Array.Reverse(arrSortTest);
+            Console.WriteLine("-----------Печать массива после сортировки---------");
+            foreach (int a in arrSortTest)
+            {
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
---
+++ kurs_arr_tusk2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace kurs_arr_tusk2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] arrMaxSearch = new int[3, 3];
+            int maxElement;
+            Console.WriteLine("Выберите вариант инициализации массива:\n1-Случайные числа (рандом от 0 до 100)\n2-Ввод вручную с клавиатуры");
+
+            byte choose = Convert.ToByte(Console.ReadLine());
+            if(choose==1)
+            {
+                Random r = new Random();
+                for (int i = 0; i < arrMaxSearch.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arrMaxSearch.GetLength(1); j++)
+                    {
+                        arrMaxSearch[i, j] = r.Next(0, 101);
+                    }
+                }
+            }
+            if(choose==2)
+            {
+                for (int i = 0; i < arrMaxSearch.GetLength(0); i++)
+                {
+                    for (int j = 0; j < arrMaxSearch.GetLength(1); j++)
+                    {
+
+                        arrMaxSearch[i, j] = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+            }
+            Console.WriteLine("----------------Печать массива----------------");
+            for (int i = 0; i < arrMaxSearch.GetLength(0); i++)
+            {
+                for (int j = 0; j < arrMaxSearch.GetLength(1); j++)
+                {
+
+                    Console.Write(arrMaxSearch[i, j]+"\t");
+                }
+                Console.WriteLine();
+            }
+            for (int i = 0; i < arrMaxSearch.GetLength(0); i++)
+            {
+                maxElement = arrMaxSearch[i,0];
+                for (int j = 0; j < arrMaxSearch.GetLength(1); j++)
+                {
+
+                    if (maxElement<arrMaxSearch[i,j])
+                    {
+                        maxElement = arrMaxSearch[i, j];
+                    }
+                }
+                Console.WriteLine($"максимальный элемент {i} строки: {maxElement}");
+            }
+        }
+    }
+}
