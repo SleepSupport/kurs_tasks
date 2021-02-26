a9ec67ef386fa68b3324fa72fbf600951059c4f4
SleepSupport <79420757+SleepSupport@users.noreply.github.com>
2021-02-26 08:37:48 +0300

    новый код

--- ConsoleApp12/Program.cs
+++ ConsoleApp12/Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
-namespace ConsoleApp12
+namespace kurs_3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double creditSumm, procent, monthPayment, paymentCreditForMonth, yearSumPayment = 0;
+            byte choose;
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Введите сумму кредита:");
+            creditSumm = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите проценты кредита:");
+            procent = Convert.ToDouble(Console.ReadLine()) / 100;
+            Console.WriteLine("Выберите тип платежа:\n 1-дифференцированные\n 2-аннуитетные");
+            choose = Convert.ToByte(Console.ReadLine());
+            Console.WriteLine("Выплаты по месяцам:");
+            monthPayment = creditSumm / 12;
+            if (choose == 1)
+            {
+                for (int i = 1; i <= 12; i++)
+                {
+
+                    paymentCreditForMonth = monthPayment + (creditSumm - (monthPayment * i)) * procent / 12;
+                    yearSumPayment += paymentCreditForMonth;
+                    Console.WriteLine($"Выплата за {i} месяц равна: " + paymentCreditForMonth);
+                }
+
+            }
+            if (choose == 2)
+            {
+                for (int i = 1; i <= 12; i++)
+                {
+                    paymentCreditForMonth = creditSumm * (procent / 12 + (procent / 12) / (Math.Pow((1 + procent / 12), 12) - 1));
+                    yearSumPayment += paymentCreditForMonth;
+                    Console.WriteLine($"Выплата за {i} месяц равна: " + paymentCreditForMonth);
+                }
+            }
+            Console.WriteLine($"Общая сумма выплат составит: {yearSumPayment}");
+            Console.WriteLine("-------------------------------------");
+
         }
     }
 }
